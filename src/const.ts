686baf7d7cf735de366bfa3a4431b29eba162dec
Vearsmon <vearsmon285537@gmail.com>
2025-09-16 21:20:52 +0500

    Начинаем программировать

---
+++ src/const.ts
@@ -0,0 +1,43 @@
+import {CardData} from './components/App.tsx';
+
+export const CardsData : CardData[] = [
+  {
+    id: 1,
+    image: 'apartment-01.jpg',
+    isPremium: true,
+    price: 120,
+    header: 'Beautiful & luxurious apartment at great location',
+    type: 'Apartment',
+    isFavorite: true,
+    rating: 1,
+  },
+  {
+    id: 2,
+    image: 'room.jpg',
+    isPremium: false,
+    price: 80,
+    header: 'Wood and stone place',
+    type: 'Room',
+    isFavorite: false,
+    rating: 2,
+  },
+  {
+    id: 3,
+    image: 'apartment-02.jpg',
+    isPremium: false,
+    price: 132,
+    header: 'Canal View Prinsengracht',
+    type: 'Apartment',
+    isFavorite: true,
+    rating: 3,
+  },
+  {
+    id: 4,
+    image: 'apartment-03.jpg',
+    isPremium: true,
+    price: 180,
+    header: 'Charming room in a shared house',
+    type: 'Apartment',
+    isFavorite: false,
+    rating: 5,
+  }];
